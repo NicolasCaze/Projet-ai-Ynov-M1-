59388cbf7233eb882e5b38058cd51a2fbfd35b2b
Nicolas <cazenicolas64@gmail.com>
2024-11-28 11:07:06 +0100

    first commit

---
+++ src/main/java/com/example/projet/ia/Projet_ia_ynov/ProjetIaYnovApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet.ia.Projet_ia_ynov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIaYnovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetIaYnovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projet/ia/Projet_ia_ynov/ProjetIaYnovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet.ia.Projet_ia_ynov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIaYnovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
